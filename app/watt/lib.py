4d2d6de2bb0f5427af9931e7183eb8a9c365308e
Sinan Teske <sinan.teske@empa.ch>
2019-02-15 18:04:59 +0100

    created definitions to get values of time, times and distance of an item in dependence of a kwh input

--- app/watt/lib.py
+++ app/watt/lib.py
@@ -5,3 +5,23 @@ def compute_energy(item1, item2, item1_quantity=1):
 
     # TODO Fix it
     return item1_quantity + item1.id + item2.id
+
+
+def get_time(kWh_input, item_to)
+    #TODO Sketch
+# will calculate the duration/distance of item_to can be used according to the kWh_input
+if() item_to.power_type == "electricity"
+
+    return time_item_to
+
+def get_times(kWh_input, item_to)
+# will calculate the number of times item_to can be used according to the kWh_input
+
+    return times_item_to
+
+
+
+
+def get_distance(kWh_input, item_to)
+
+    return km_item_to
