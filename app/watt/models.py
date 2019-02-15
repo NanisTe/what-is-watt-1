18cf2c8352f1939b71616c37c5f22d73cd451abe
Sinan Teske <sinan.teske@empa.ch>
2019-02-15 18:19:19 +0100

    Added fields to the Item Class and created a Converter class which should define unit conversions with an efficiency factor.

--- app/watt/models.py
+++ app/watt/models.py
@@ -6,10 +6,29 @@ class ItemManager(models.Manager):
 
 class Item(models.Model):
     name = models.CharField(max_length=200)
-    power = models.PositiveIntegerField()
-    power_type = models.CharField(max_length=200)
+    power = models.PositiveIntegerField() # kWh/h from CSV or Wh/h
+    times_per_hour = models.PositiveIntegerField() # standard usage value
+    power_type = models.CharField(max_length=200) # type, electricity, gas, gasoline, diesel, heat,
+    unit = models.CharField(max_length=200)
+    default_usage = models.FloatField()
+    url_data = models.CharField()
 
     objects = ItemManager()
 
     def __str__(self):
         return self.name
+
+class Converter(models.Model):
+    name = models.CharField(max_length=200)
+    unit_in = models.CharField(max_length=200) # TODO define a class of units: with unit type and conversion methods
+    unit_out = models.CharField(max_length=200) # TODO define a class of units
+    conversion_factor = models.FloatField()
+    unit = models.CharField(max_length=200)
+
+    objects = ItemManager()
+
+    def __str__(self):
+        return self.name
+
+
+#TODO define class of kwh to distinguish between electricity and other energy carriers.
